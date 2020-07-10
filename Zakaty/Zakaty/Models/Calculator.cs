508b5abb7cc588db463a02f59260490ae256af82
SOAL Abdeldjallil <abdeldjallil93@hotmail.fr>
2020-07-10 13:14:36 +0100

    first build

---
+++ Zakaty/Zakaty/Models/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zakaty.Models
+{
+    class Calculator
+    {
+        public double GoldPrice { get; set; }
+        public double SilverPrice { get; set; }
+        public double NaturallyCropsPrice { get; set; }
+        public double IrrigatedWaterCropsPrice { get; set; }
+        public double CashOnHand { get; set; }
+        public double LoanCash { get; set; }
+        public double SharesProfit { get; set; }
+        public double SaleableStock { get; set; }
+        public double LiabilitiesDebts { get; set; }
+        public double PriceOfLand { get; set; }
+
+
+
+        public double All()
+        {
+            double all = GoldPrice + SilverPrice + NaturallyCropsPrice + IrrigatedWaterCropsPrice + CashOnHand +
+                         LoanCash + LiabilitiesDebts + SaleableStock + SharesProfit + PriceOfLand;
+            return all;
+        }
+    }
+}
